402fcd970a39ee1663f1575633c541018fa66c5a
Ryan Pereira <ryanprod.dev@gmail.com>
2026-09-03 18:50:12 -0300

    Tipos primitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+
+public class TiposPrimitivos {
+    byte ex1 = 127; // numeros inteiros muito pequenos
+    short ex2 = 32767; // numeros inteiros pequenos
+    int ex3 = 10; // numeros inteiros menores
+    long ex4 = 11L; // numeros inteiro maiores
+    float ex5 = 12.0F; // numero flutuante pequeno
+    double ex6 = 13.9D; // numeros futuantes grandes
+    char ex7 = 'M'; // 1 caractere
+    boolean ex8 = false; // true ou false
+
+}
