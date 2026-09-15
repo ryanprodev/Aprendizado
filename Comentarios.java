ca1e4104a7861996ae3053b9a5f1bdb803adad03
Ryan Pereira <ryanprod.dev@gmail.com>
2026-09-15 17:39:31 -0300

    feat(fundamentos): exemplo de comentários

---
+++ Comentarios.java
@@ -0,0 +1,19 @@
+package A_Fundamentos;
+
+public class Comentarios {
+    static void main() {
+        // 1. Duas barras para comentário de uma linha só
+        System.out.println("Testando comentários 1");
+
+        /* 2.
+              Uma barra com asterisco para comentário de mais linhas */
+        System.out.println("Testando comentários 2");
+
+        /**
+         * 3. Uma barra com 2 asteriscos (JavaDoc)
+         * é usado para fazer documentações oficiais.
+         */
+        System.out.println("Testando comentários 3");
+
+    }
+}
