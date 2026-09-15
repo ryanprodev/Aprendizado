ff4635273c5451a93466e0e2ff872c31f02de1f7
Ryan Pereira <ryanprod.dev@gmail.com>
2026-09-15 17:18:41 -0300

    feat(fundamentos):adiciona exemplo de criação de variáveis

---
+++ Variaveis.java
@@ -0,0 +1,10 @@
+package A_Fundamentos;
+
+public class Variaveis {
+    static void main() {
+        // 1. Cria uma variável e dá um valor a ela
+        String nome = "Ryan";
+        // 2. Imprime o valor no console
+        System.out.println("Olá, " + nome + "!"); // Olá, Ryan!
+    }
+}
